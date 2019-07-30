02f4d08f8784011cd0da42f53fcfff14dedc8495
NickVanheer <nickvanheer@live.be>
2019-07-31 05:23:32 +0900

    Potion Pickup bugfix to make it a regular pickup as a trigger once on the ground

--- Assets/Scripts/Player/PotionPickup.cs
+++ Assets/Scripts/Player/PotionPickup.cs
@@ -7,12 +7,20 @@ public class PotionPickup : MonoBehaviour {
     public float FallSpeed = 2;
     public void FixedUpdate()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.down * FallSpeed);
+        if (!GetComponent<BoxCollider>().isTrigger)
+            GetComponent<Rigidbody>().AddForce(Vector3.down * FallSpeed);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        //If the potion has fallen to the floor, make it a trigger and set it to kinematic so we can pick it up well.
+        GetComponent<BoxCollider>().isTrigger = true;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
         {
             GameManager.Instance.RestoreAllHP(30, true);
             Destroy(this.gameObject);
